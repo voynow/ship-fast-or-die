154731213e21800213ad3331763d233d7d77886e
voynow <voynow99@gmail.com>
2025-02-16 18:28:33 -0500

    cors fix

--- src/ship_fast_or_die/main.py
+++ src/ship_fast_or_die/main.py
@@ -20,7 +20,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://ship-fast-or-die.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
